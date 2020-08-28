4c71e4e05e860e4e87c51ac3a16ac58a968b38be
Matthew Barber <quitesimplymatt@gmail.com>
2020-08-28 19:27:23 +0100

    readme as pypi long desc

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name="sts-pylib",
     description="NIST's Statistical Test Suite bindings",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Matthew Barber",
     py_modules=["sts"],
     cffi_modules=["ext_build.py:ffi"],
