f357111aeb28f01cc1fd237bfaca669be2f0f51f
Matthew Barber <quitesimplymatt@gmail.com>
2020-09-03 14:35:34 +0100

    Revert "added packages directive"
    This reverts commit 5e71be44fe68318f84123965b0aa3eb5bf7698dc.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author_email="quitesimplymatt@gmail.com",
     url="https://github.com/Honno/sts-pylib",
     include_package_data=True,
-    packages = ["."],
     project_urls={
         "Documentation": "https://sts-pylib.readthedocs.io/",
         "Issue Tracker": "https://github.com/Honno/sts-pylib/issues",
